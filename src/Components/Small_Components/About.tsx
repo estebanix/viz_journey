b85fc6e3e5b6e7ebc9f4b7b86a8860893c3cf0b1
estebanix <dakisunshine@gmail.com>
2023-08-16 21:12:04 +0200

    -m "axis"

--- src/Components/Small_Components/About.tsx
+++ src/Components/Small_Components/About.tsx
@@ -1,36 +1,41 @@
 import { useRef, useEffect, useState } from "react";
-import {select, line, curveCardinal} from "d3";
+import {select, line, curveCardinal, axisBottom, axisRight, scaleLinear} from "d3";
 
 export default function About(){
     const svgRef = useRef();
-    const [data, setData] = useState([55, 29, 83, 40, 54])
+    const [data, setData] = useState([20,30, 50, 22, 69])
     useEffect(() => {
         const svg = select(svgRef.current);
+        const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 300])
+        const yScale = scaleLinear().domain([0, 75]).range([150, 0])
+
+        const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
+        svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+        
+        const yAxis = axisRight(yScale);
+        svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
         const myLine = line()
-            .x((value, index) => index * 50)
-            .y(value => value)
+            .x((value, index) => xScale(index))
+            .y(yScale)
             .curve(curveCardinal)
         svg
-        .selectAll("path")
+        .selectAll(".line")
         .data([data])
         .join("path")
-        .attr("d", value => myLine(value))
+        .attr("class", "line")
+        .attr("d", myLine)
         .attr("fill", "none")
         .attr("stroke", "blue")
-        /*svg
-        .selectAll("circle")
-        .data(data)
-        .join("circle")
-            .attr("r", value => value)
-            .attr("stroke", "red")
-            .attr("cy", value => value * 2)
-            .attr("cx", value => value * 3)
-            .attr("fill", value => `rgba(148, 028, 128, 0.${value})`)*/
     }, [data])
+
     return(
-        <main>
-            <svg ref={svgRef}></svg>
-            <button onClick={() => setData(data.map(value => value * 1.05))}>CLICK</button>
+        <main className="main-contact--container">
+            <svg ref={svgRef}>
+                <g className="x-axis" />
+                <g className="y-axis" />
+            </svg>
+            <button onClick={() => setData(data.map((dat) => dat + 5))}>click</button>
         </main>
     );
 }
