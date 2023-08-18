4e4259171517bbb4999eebd11c7a70c6246874a9
estebanix <dakisunshine@gmail.com>
2023-08-18 21:13:10 +0200

    new dataset added through

--- src/Components/Small_Components/About.tsx
+++ src/Components/Small_Components/About.tsx
@@ -1,31 +1,32 @@
-import { useRef, useEffect, useState} from "react";
+import { useRef, useEffect, useState, useContext} from "react";
 import {select, axisBottom, axisRight, scaleLinear, scaleBand} from "d3";
+import { Context } from "../../Context/Context";
 
 export default function About(){
     const svgRef = useRef();
+    const {vizData} = useContext(Context);
     const [data, setData] = useState([{x: 20, y: 34, label: "dot1"},{x: 10, y: 44, label: "dot2"},{x: 50, y: 134, label: "dot3"}])
     const [rangePostion, setRangePosition] = useState(1)
 
     useEffect(() => {
         const svg = select(svgRef.current);
-        const xScale = scaleBand().domain(data.map((dat, index) => index)).range([0, 300]).padding(0.5)
-        const yScale = scaleLinear().domain([0, 75]).range([150, 0])
-        const colorScale = scaleLinear().domain([40, 75]).range(["blue", "pink"]).clamp(true)
+        const xScale = scaleBand().domain(data.map((dat, index) => index)).range([0, 600]).padding(0.5)
+        const yScale = scaleLinear().domain([0, 150]).range([300, 0])
 
         const xAxis = axisBottom(xScale).ticks(data.length);
-        svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+        svg.select(".x-axis").style("transform", "translateY(300px)").call(xAxis);
         
         const yAxis = axisRight(yScale);
-        svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+        svg.select(".y-axis").style("transform", "translateX(600px)").call(yAxis);
 
         svg
         .selectAll("circle")
-        .data(data)
+        .data(vizData)
         .join("circle")
         .attr("fill", `rgba(205, 92, 92, 0.${rangePostion})`)
-        .attr("cx", value => value.x * rangePostion / 4)
-        .attr("cy", value => 150 - value.y * rangePostion / 20)
-        .attr("r", "10")
+        .attr("cx", value => value.x / (10 * rangePostion))
+        .attr("cy", value => value.y * 3 )
+        .attr("r", "5")
         
         .on("mouseover", (event, value) => {
             const [x, y] = [event.pageX, event.pageY];
@@ -40,7 +41,7 @@ export default function About(){
             svg.select(".tooltip").remove();
           });
 
-    }, [data, rangePostion])
+    }, [vizData, rangePostion])
 
     const showFlow = (e) => {
         setRangePosition(e.target.value)
@@ -48,7 +49,7 @@ export default function About(){
 
     return(
         <main className="main-contact--container">
-            <svg ref={svgRef}>
+            <svg ref={svgRef} height={300}>
                 <g className="x-axis" />
                 <g className="y-axis" />
             </svg>
--- src/Context/Context.tsx
+++ src/Context/Context.tsx
@@ -1,21 +1,26 @@
 import { createContext, useState } from 'react';
 import Articles from "../Datas/articles.json";
+import VizDat from "../Datas/data.json";
 
 export interface ContextType {
     articlesData: any[];
     setArticlesData: (data: any) => void; 
+    vizData: any[];
+    setVizData: (data: any) => void; 
 }
 
 export const Context = createContext<ContextType>({} as ContextType);
 
 const ContextProvider = (props: any) => {
     const [articlesData, setArticlesData] = useState(Articles);
+    const [vizData, setVizData] = useState(VizDat);
     
     return (
         <Context.Provider
             value={{
                 articlesData,
-                setArticlesData
+                setArticlesData,
+                vizData
             }}
         >
             {props.children}
