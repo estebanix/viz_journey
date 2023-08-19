d4f95368ffbd945900d1cbbd5131dfc447cc263e
estebanix <dakisunshine@gmail.com>
2023-08-19 16:27:46 +0200

    styling of current post page and ovewflowing of post list

--- src/Components/Small_Components/CurrentPost.tsx
+++ src/Components/Small_Components/CurrentPost.tsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { Context } from '../../Context/Context';
 import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
 export default function CurrentPost() {
     const { postName } = useParams();
@@ -15,7 +17,9 @@ export default function CurrentPost() {
     return (
         <div className='current-post--container'>
             <h1>{post.title}</h1>
-            <p>{post.content}</p>
+            <p className='current-post--time'><FontAwesomeIcon icon={faClock} /> {post.time}</p>
+            <p className='current-post--p'>{post.content}</p>
+            <img className='current-post--img' src={post.img} />
         </div>
     );
 }
