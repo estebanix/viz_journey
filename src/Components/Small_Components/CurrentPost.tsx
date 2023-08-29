43fb64a80d5017bad2372374098018c41fbfe596
estebanix <dakisunshine@gmail.com>
2023-08-29 17:58:45 +0200

    markdown added

--- src/Components/Small_Components/CurrentPost.tsx
+++ src/Components/Small_Components/CurrentPost.tsx
@@ -3,6 +3,7 @@ import { Context } from '../../Context/Context';
 import { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 export default function CurrentPost() {
     const { postName } = useParams();
@@ -18,7 +19,9 @@ export default function CurrentPost() {
         <div className='current-post--container'>
             <h1>{post.title}</h1>
             <p className='current-post--time'><FontAwesomeIcon icon={faClock} /> {post.time}</p>
-            <p className='current-post--p'>{post.content}</p>
+            <ReactMarkdown className='current-post--p'>
+                {post.content}
+            </ReactMarkdown>
             <img className='current-post--img' src={post.img} />
         </div>
     );
